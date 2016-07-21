48c7d45fa656c7646c5f3e087484dabe51e3b798
Nathan Walker <walkerrunpdx@gmail.com>
2016-07-21 09:19:19 -0700

    feat: iOS working

--- colorslider.ios.ts
+++ colorslider.ios.ts
@@ -7,25 +7,17 @@ export class Slider extends ContentView {
  
   private _ios: any = ColorSlider;
 
-    get ios(): any {
-        return this._ios;
-    }
-
-    get _nativeView(): any {
-        return this._ios;
-    }
-
   constructor() {
     super();
     this._ios = new ColorSlider();
     this._ios.frame = CGRectMake(0, 0, 12, 150);
-    // this._ios.frame = ColorSlider.alloc().initWithFrame(CGRectMake(0, 0, 12, 150));
-    // topmost().ios.controller.view.addSubview(slider); 
-    // console.log(ColorSlider);
-    // if (ColorSlider) {
-    //   for (let key in ColorSlider) {
-    //     console.log(key);
-    //   }
-    // }
-  }   
+  }  
+
+  get ios(): any {
+    return this._ios;
+  }
+
+  get _nativeView(): any {
+    return this._ios;
+  } 
 }
