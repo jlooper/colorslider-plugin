406f220d217ad6a2d8e8999573652487e2d66cef
jlooper <jen.looper@gmail.com>
2016-07-23 22:42:27 -0400

    adding some params for iOS. Touch the slider and see the previewMode - slide finger horizontally for saturation, vertically for hue

--- colorslider.ios.ts
+++ colorslider.ios.ts
@@ -1,12 +1,20 @@
 import {topmost} from 'ui/frame';
 import {ContentView} from 'ui/content-view';
-
+import {Color} from "color";
+/*
+orientation: String;
+    previewEnabled: Boolean;
+    borderWidth: Number;
+    borderColor: String;
+    */
 declare var ColorSlider: any, CGRectMake: any;
 
 export class Slider extends ContentView {
  
   private _ios: any = ColorSlider;
 
+  public static colorChangeEvent = "changedColor";
+    
   constructor() {
     super();
     this._ios = new ColorSlider();
@@ -20,4 +28,18 @@ export class Slider extends ContentView {
   get _nativeView(): any {
     return this._ios;
   } 
+  
+  set orientation(value: string) {
+    this._ios.orientation = +value;
+  }
+
+  set previewEnabled(value: boolean) {
+    this._ios.previewEnabled = +value;
+  }
+
+  set borderWidth(value: number) {
+    this._ios.borderWidth = +value;
+  }
+
+  
 }
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -2,17 +2,28 @@ import {Observable} from 'data/observable';
 import {ObservableArray} from "data/observable-array"
 
 export class HelloWorldModel extends Observable {
+  //android
   colorList;
   thumbHeight: number;
   barHeight: number;
   showAlphaBar: boolean;
   alphaBar:number;
+  //ios
+  orientation: string;
+  borderWidth: number;
+  previewEnabled: boolean;
   constructor() {
     super();
+    //android
     this.thumbHeight = 50;
     this.barHeight = 20;
     this.alphaBar = 0;
     this.showAlphaBar = true;
-    this.colorList = new ObservableArray(["#F44336", "#9C27B0", "#3F51B5", "#03A9F4", "#009688", "#8BC34A", "#FFEB3B", "#FF9800", "#795548", "#607D8B"])
+    this.colorList = new ObservableArray(["#F44336", "#9C27B0", "#3F51B5", "#03A9F4", "#009688", "#8BC34A", "#FFEB3B", "#FF9800", "#795548", "#607D8B"]);
+    //ios
+    this.orientation = ".Vertical";
+    this.borderWidth = 2;
+    this.previewEnabled = true;
+
   }
 }
